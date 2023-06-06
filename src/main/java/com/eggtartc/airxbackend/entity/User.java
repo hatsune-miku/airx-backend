787f0b3a37ad67e5c408e0e5fae8b7b0a40ff186
eggtart <l2a1knla@gmail.com>
2023-06-06 14:08:11 -0230

    fix timezone

--- src/main/java/com/eggtartc/airxbackend/entity/User.java
+++ src/main/java/com/eggtartc/airxbackend/entity/User.java
@@ -6,7 +6,10 @@ import lombok.*;
 import org.hibernate.Hibernate;
 
 import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Objects;
+import java.util.TimeZone;
 
 @Entity
 @NoArgsConstructor
@@ -49,7 +52,9 @@ public class User {
     }
 
     public String calculateSalt(String secret) {
-        return HashUtil.sha256(secret + id + uid + name + validBefore.getTime() + secret + password);
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.setTime(validBefore);
+        return HashUtil.sha256(secret + id + uid + name + calendar.getTime().getTime() + secret + password);
     }
 
     public boolean isSaltValid(String secret) {
@@ -59,4 +64,5 @@ public class User {
     public void correctSalt(String secret) {
         salt = calculateSalt(secret);
     }
+
 }
